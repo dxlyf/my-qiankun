ba07d247a2600a25516b4a3f4562e50670399ae4
fanyonglong <yonglong.fan@xfxb.net>
2021-12-10 14:46:52 +0800

    'f'

--- packages/main/src/layouts/BasicLayout.tsx
+++ packages/main/src/layouts/BasicLayout.tsx
@@ -3,6 +3,7 @@ import React,{useState} from 'react';
 
 let  BasicLayout=(props:any)=>{
     return <div>
+        <img src="/logo192.png" />
         <div>base</div>
         {props.children}
     </div>
